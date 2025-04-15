42f41df23e4c1adb1fdd78322ac70ba4c16e46d6
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-16 00:05:10 +0530

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -2,5 +2,5 @@ def handler(event, context):
     print("Hello World")
     return {
         'statusCode': 200,
-        'body': 'Hello Balu your lambda function is working'
+        'body': 'Hello World'
     }
